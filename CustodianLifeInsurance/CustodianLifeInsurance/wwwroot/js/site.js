0dd201a3ed37e4138bdeae1a11336dde94f67880
seye007 <dronesheye@gmail.com>
2023-07-04 08:24:24 +0100

    updated version

--- CustodianLifeInsurance/CustodianLifeInsurance/wwwroot/js/site.js
+++ CustodianLifeInsurance/CustodianLifeInsurance/wwwroot/js/site.js
@@ -12,3 +12,16 @@ selectElement.addEventListener("focus", function () {
 selectElement.addEventListener("blur", function () {
   selectContainer.classList.remove("show-dropdown");
 });
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+  var miniNavElement = document.getElementsByClassName('nav')[0];
+  var majorNavElement = document.getElementsByClassName('navbar')[0];
+  var miniNavHeight = miniNavElement.offsetHeight || miniNavElement.clientHeight;
+  var majorNavHeight = majorNavElement.offsetHeight || majorNavElement.clientHeight;
+  var heroSection = miniNavHeight + majorNavHeight;
+  var viewportHeight = window.innerHeight;
+  document.getElementsByClassName('carousel-item')[0].style.height = viewportHeight - heroSection + 'px';
+  document.getElementsByClassName('carousel-item')[1].style.height = viewportHeight - heroSection + 'px';
+});
